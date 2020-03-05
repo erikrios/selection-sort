221432703e227dda321562cba881e698bccc8e44
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-05 12:03:53 +0700

    Add the example code of Selection Sort

---
+++ main.js
@@ -0,0 +1,35 @@
+function selectionSort(numbers) {
+	
+	for (var i = 0; i < numbers.length;i++) {
+		
+		var minValue = numbers[i];
+		var minValueIndex = i;
+		
+		for (var j = i; j < numbers.length - 1; j++) {
+			if (minValue > numbers[j + 1]) {
+				minValue = numbers[j + 1];
+				minValueIndex = j + 1;
+			}
+		}
+		
+		if (minValue != numbers[i]) {
+			// Swap the data
+			var temp = numbers[i];
+			numbers[i] = numbers[minValueIndex];
+			numbers[minValueIndex] = temp;
+		}
+	}
+	
+	return numbers;
+}
+
+var numbers = [8, 9, 5, 2, 3, 9, 10, 1, 3, 7, 2];
+
+var sortedNumbers = selectionSort(numbers);
+
+var result = "";
+
+for (var i = 0; i < sortedNumbers.length; i++)
+	result += sortedNumbers[i] + " ";
+
+console.log(result);
